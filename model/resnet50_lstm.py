5ef0d6aba5a5e3070b4e31822b3a3c8558771aa7
Rohityadav2005 <rohit2205x@gmail.com>
2026-04-10 18:54:40 +0530

    Recti

--- model/resnet50_lstm.py
+++ model/resnet50_lstm.py
@@ -1,11 +1,11 @@
 import torch.nn as nn 
 
-class ResNetLSTM(nn.Module):
-    def __init__(self,resnet_base,hidden_dim = 512,num_layers = 2):
-        super(ResNetLSTM,self).__init__()
-        self.resnet = resnet_base
+class ShuffleNetLSTM(nn.Module):
+    def __init__(self,shufflenet_base,hidden_dim = 512,num_layers = 2):
+        super(ShuffleNetLSTM,self).__init__()
+        self.shufflenet = shufflenet_base
 
-        self.lstm = nn.LSTM(input_size=2048,
+        self.lstm = nn.LSTM(input_size=1024,
                             hidden_size=hidden_dim,
                             num_layers=num_layers,
                             batch_first=True)
@@ -16,7 +16,7 @@ class ResNetLSTM(nn.Module):
         batch_size,seq_len,c,h,w = x.shape
 
         x = x.view(batch_size*seq_len,c,h,w)
-        features = self.resnet(x)
+        features = self.shufflenet(x)
         features = features.view(batch_size,seq_len,-1)
         lstm_out,(hidden,cell) = self.lstm(features)
         last_frame_feat = lstm_out[:,-1,:]
